a23b4b5492262aa5cc15dabbdd1774c8566e81a2
codr7 <andreas.gone.wild@gmail.com>
2019-09-23 22:05:03 +0200

    added opt types

--- src/type.lisp
+++ src/type.lisp
@@ -1,23 +1,39 @@
 (in-package lila)
 
 (defclass lila-type ()
-  ((id :initarg :id :reader id)))
+  ((id :initarg :id :reader id)
+   (opt-type :initform nil :reader opt-type)))
 
 (defmacro define-type (id (&rest parents))
   (let* ((ids (string-downcase (symbol-name id)))
-         (type-id (symf "~a-type" ids)))
+         (type-id (symf "~a-type" ids))
+         forms)
     (unless parents
       (push 'lila parents))
+
+    (push `(defvar ,type-id
+             (make-instance ',type-id :id ,(make-id (caps! ids))))
+          forms)
+    
+    (push `(defclass ,type-id (,@(mapcar (lambda (p)
+                                           (symf "~a-type"
+                                                 (string-downcase (symbol-name p))))
+                                         parents))
+             ())
+          forms)
+
+    (unless (eq type-id 'none-type)
+      (let ((opt-ids (format nil "~a?" ids))
+            (opt-type-id (symf "~a-opt-type" ids)))
+        (push `(setf (slot-value ,type-id 'opt-type)
+                     (make-instance ',opt-type-id :id ,(make-id (caps! opt-ids))))
+              forms)
+        (push `(defclass ,opt-type-id (,type-id none-type)
+                 ())
+              forms)))
     
     `(progn
-       (defclass ,type-id (,@(mapcar (lambda (p)
-                                       (symf "~a-type"
-                                             (string-downcase (symbol-name p))))
-                                     parents))
-         ())
-       
-       (defvar ,type-id
-         (make-instance ',type-id :id ,(make-id (caps! ids)))))))
+       ,@forms)))
 
 (defmethod print-object ((v lila-type) out)
   (write-string (symbol-name (id v)) out))
