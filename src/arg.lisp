1d20d7cc0f413dfb1a88a0550d6dc0e018fed3f5
codr7 <andreas.gone.wild@gmail.com>
2019-09-27 23:10:47 +0200

    todo

--- src/arg.lisp
+++ src/arg.lisp
@@ -6,6 +6,6 @@
       a))
 
 (defun get-arg-type (a)
-  (find-class (if (consp a)
-                  (get-type-id (if (pair? a) (rest a) (second a)))
-                  'any-type)))
+  (if (consp a)
+      (get-type-id (if (pair? a) (rest a) (second a)))
+      'any-type))
