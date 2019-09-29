0a926572866253d280c963e216f655639b4368e8
codr7 <andreas.gone.wild@gmail.com>
2019-09-29 14:52:49 +0200

    optimization level

--- src/compile.lisp
+++ src/compile.lisp
@@ -3,10 +3,10 @@
 (defvar *speed* 4)
 
 (defvar *debug-levels*
-  #(3 2 3 2 1 2 1 0 1 0))
+  #(3 2 1 0 2 1 0 1 0 0))
 
 (defvar *safety-levels*
-  #(3 3 2 2 2 1 1 1 0 0))
+  #(3 3 3 3 2 2 2 1 1 0))
 
 (defun lila-compile (vals)
   (let ((code (emit-vals vals)))
