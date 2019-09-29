3abb75a2c6f49fc73862e2497d904f635e786452
codr7 <andreas.gone.wild@gmail.com>
2019-09-29 17:22:14 +0200

    simplified optimization level

--- src/compile.lisp
+++ src/compile.lisp
@@ -2,14 +2,11 @@
 
 (defvar *speed* 0)
 
-(defvar *safety-levels*
-  #(3 3 3 2 2 2 1 1 1 0))
-
 (defun lila-compile (vals)
   (let ((code (emit-vals vals)))
     ;(format t "~a~%" code)
     (compile nil `(lambda ()
                     (declare (optimize (speed ,(floor *speed* 3))
                                        (debug 3)
-                                       (safety ,(aref *safety-levels* *speed*))))
+                                       (safety ,(floor (- 9 *speed*) 3))))
                     ,@code))))
