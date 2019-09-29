4e621bcfa944e6be83db9f590f7b91e252cee19f
codr7 <andreas.gone.wild@gmail.com>
2019-09-29 17:20:15 +0200

    default speed

--- src/compile.lisp
+++ src/compile.lisp
@@ -2,17 +2,14 @@
 
 (defvar *speed* 0)
 
-(defvar *debug-levels*
-  #(3 2 1 0 2 1 0 1 0 0))
-
 (defvar *safety-levels*
-  #(3 3 3 3 2 2 2 1 1 0))
+  #(3 3 3 2 2 2 1 1 1 0))
 
 (defun lila-compile (vals)
   (let ((code (emit-vals vals)))
     ;(format t "~a~%" code)
     (compile nil `(lambda ()
                     (declare (optimize (speed ,(floor *speed* 3))
-                                       (debug ,(aref *debug-levels* *speed*))
+                                       (debug 3)
                                        (safety ,(aref *safety-levels* *speed*))))
                     ,@code))))
